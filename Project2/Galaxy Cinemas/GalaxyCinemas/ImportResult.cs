0256a633c579e730749b20ecc02c5288a797f308
unknown <blueocean2223@gmail.com>
2018-11-30 00:11:47 +1300

    First commit

---
+++ Project2/Galaxy Cinemas/GalaxyCinemas/ImportResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GalaxyCinemas
+{
+    public class ImportResult
+    {
+       
+        public int TotalRows
+        {
+            get;
+            set;
+        }
+        public int ImportedRows
+        {
+            get;
+            set;
+        }
+        public int FailedRows
+        {
+            get;
+            set;
+        }
+        private List<string> errorMessages=new List<string>();
+        public List<string> ErrorMessages
+        {
+            get
+            {
+              return  errorMessages;
+            }
+        }
+        public ImportResult()
+        {
+            TotalRows = 0;
+            ImportedRows = 0;
+            FailedRows = 0;
+            errorMessages.Clear();
+        }
+       
+      
+
+       
+    }
+}
---
+++ Project2/Galaxy Cinemas/GalaxyCinemas/SessionImporter.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using Common;
+using Common.Business_Objects;
+using GalaxyCinemas.DataLayer;
+
+namespace GalaxyCinemas
+{
+    public class SessionImporter : BaseImporter
+    {
+        public SessionImporter(string filename) : base(filename)
+        {
+        }
+
+        /// <summary>
+        /// Import session file. Filename has been provided in the constructor.
+        /// </summary>
+        public override void Import(object o)
+        {
+            // Initialise progress to zero for progress bar.
+            Progress = 0f;
+            ImportResult results = new ImportResult();
+            try
+            {
+                // Read file
+                string fileData = null;
+                using (StreamReader reader = File.OpenText(fileName))
+                {
+                    fileData = reader.ReadToEnd();
+                }
+                string[] lines = fileData.Replace("\r\n", "\n").Replace("\r", "\n").Split('\n'); // To deal with Windows, Mac and Linux line endings the same.
+
+                // Check if first line is column names.
+                string firstLine = lines[0];
+                string[] columns = firstLine.Split(',');
+                if (columns.Length == 4)
+                {
+                    if ((columns[0].ToLower().Trim() == "sessionid") && (columns[1].ToLower().Trim() == "movieid") && (columns[2].ToLower().Trim() == "sessiondate") && (columns[3].ToLower().Trim()=="cinemanumber"))
+                    {
+                        lines[0] = "";
+                    }
+
+                }
+                // Line count and line numbers to allow progress tracking.
+                int lineCount = lines.Length;
+                int lineNum = 1;
+
+                // Get all movies. These will be used to check that MovieIDs are valid.
+                List<Movie> movies = DataLayer.DataLayer.GetAllMovies();
+
+                foreach (string line in lines)
+                {
+                    // Check whether we need to stop after importing each line.
+                    if (Stop)
+                    {
+                        return;
+                    }
+
+                    try
+                    {
+                        // Just to make it slow enough to testing stopping functionality.
+                        Thread.Sleep(500);
+
+                        // Update progress of import.
+                        Progress = lineNum / lineCount;
+                        RaiseProgressChanged();
+
+
+
+
+                        // Skip blank lines
+
+                        if (line == "")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            results.TotalRows++;
+                        }
+
+                        // Break up line by commas, each item in array will be one column.
+                        columns = line.Split(',');
+                        if (columns.Length != 4)
+                        {
+                            results.FailedRows++;
+                            results.ErrorMessages.Add(string.Format("Line {0}: Wrong number of columns.", lineNum));
+                            continue;
+                        }
+
+                        // Check the format of data, and update ImportResult accordingly.
+                        
+                        // Check movie ID.
+                        int movieID = 0;
+                        if (!int.TryParse(columns[1].Trim(), out movieID))
+                        {
+                            results.FailedRows++;
+                            results.ErrorMessages.Add(string.Format("Line {0}: MovieID is not a number.", lineNum));
+                            continue;
+                        }
+                        if (movies.Count(m => m.MovieID == movieID) < 1)
+                        {
+                            results.FailedRows++;
+                            results.ErrorMessages.Add(string.Format("Line {0}: MovieID not found in movie database.", lineNum));
+                            continue;
+                        }
+                        // Check session date/time.
+                        DateTime sessionDate = DateTime.MinValue;
+                        if (!DateTime.TryParse(columns[2].Trim(), out sessionDate))
+                        {
+                            results.FailedRows++;
+                            results.ErrorMessages.Add(string.Format("Line{0}: Session date is not a date/time", lineNum));
+                            continue;
+                        }
+
+                        // Check session ID.
+                        int sessionID;
+                        if (!int.TryParse(columns[0].Trim(), out sessionID))
+                        {
+                            results.FailedRows++;
+                            results.ErrorMessages.Add(string.Format("Line {0}: SessionID is not a number.", lineNum));
+                            continue;
+                        }
+
+
+
+                        // Check cinema number.
+                        byte cinemaNumber;
+                        if (!byte.TryParse(columns[3].Trim(), out cinemaNumber))
+                        {
+                            results.FailedRows++;
+                            results.ErrorMessages.Add(string.Format("Line {0}: CinemaNumber is not a number.", lineNum));
+                            continue;
+                        }
+
+                        if (cinemaNumber < 1)
+                        {
+                            results.FailedRows++;
+                            results.ErrorMessages.Add(string.Format("Line {0}: Cinema number invalid-cinema number must be positive", lineNum));
+                            continue;
+                        }
+
+
+
+
+
+                        // Insert/update DB if okay.
+
+                        Session sessionToUpdate = DataLayer.DataLayer.GetSessionByID(sessionID);
+                        if(sessionToUpdate==null)
+                        {
+                            Session sessionToAdd = new Session { SessionID = sessionID, MovieID = movieID, SessionDate = sessionDate, CinemaNumber = cinemaNumber };
+                            DataLayer.DataLayer.AddSession(sessionToAdd);
+                        }
+                        else
+                        {
+                            sessionToUpdate.MovieID = movieID;
+                            sessionToUpdate.SessionDate = sessionDate;
+                            sessionToUpdate.CinemaNumber = cinemaNumber;
+                            DataLayer.DataLayer.UpdateSession(sessionToUpdate);
+                        }
+
+
+
+
+                        results.ImportedRows++;
+
+
+
+
+
+
+
+
+                    }
+                    catch (System.Data.Common.DbException dbEx)
+                    {
+                        results.FailedRows++;
+                        results.ErrorMessages.Add(string.Format("Line {0}: Database error occurred updating data.", lineNum));
+                    }
+                    finally
+                    {
+                        lineNum++;
+                    }
+                }
+            }
+            catch (System.IO.IOException)
+            {
+                results.ErrorMessages.Add("Error occurred opening file. Please check that the file exists and that you have permissions to open it.");
+            }
+            catch (Exception)
+            {
+                results.ErrorMessages.Add("An unknown error occurred during importing.");
+            }
+            finally
+            {
+                // Do callback to end import.
+                RaiseCompleted(results);
+            }
+
+        
+        }
+
+    }
+}
