5e32e470199a02fafc291bc15d7e19f2a32523c0
unknown <blueocean2223@gmail.com>
2018-11-30 00:23:40 +1300

    First commit

---
+++ Project2/Galaxy Cinemas/Common/Business_Objects/Booking.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Business_Objects
+{
+   public class Booking
+    {
+        public int BookingNumber
+        {
+            get;
+            set;
+        }
+        public int SessionID
+        {
+            get;
+            set;
+        }
+        public DateTime SessionDate
+        {
+            get;
+            set;
+        }
+        public int Quantity
+        {
+            get;
+            set;
+        }
+        public string Special
+        {
+            get;
+            set;
+        }
+        public decimal Discount
+        {
+            get;
+            set;
+        }
+        public decimal OriginalPrice
+        {
+            get;
+            set;
+        }
+        public decimal FinalPrice
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Project2/Galaxy Cinemas/Plugins/Pricing Plugins/TuesdaySpecialPlugin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Common;
+using Common.Business_Objects;
+
+namespace GalaxyCinemas
+{
+    public class TuesdaySpecialPlugin:ISpecialPlugin
+    {
+        public bool CalculateSpecial(Booking booking, ref string specialName, ref decimal specialPrice)
+        {
+            // If not Tuesday, not applicable.
+            DayOfWeek day=booking.SessionDate.DayOfWeek;
+            if(day==DayOfWeek.Tuesday)
+            {
+                decimal basePrice = (booking.OriginalPrice) / (booking.Quantity);
+                decimal discountedPrice = 0;
+                if(booking.Quantity<=5)
+                {
+                    discountedPrice = 11 * (booking.Quantity);
+                }
+                else
+                {
+                    discountedPrice = 11 * 5 + (basePrice * ((booking.Quantity) - 5));
+                }
+                if(discountedPrice<(booking.OriginalPrice))
+                {
+                    specialPrice = discountedPrice;
+                    specialName = "Tuesday Special";
+                }
+                return true;
+
+            }
+            else { return false; }
+
+
+            // Get base unit price.
+
+            // Calculate the discounted price that we would offer.
+    
+          
+           
+
+            // If this discount is applicable, set specialName and specialPrice to our name and price.
+            
+        }
+    }
+}
